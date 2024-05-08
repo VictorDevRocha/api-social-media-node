141fb5c31dd133185a94ec6d1143e9a8127b2262
Victor Rocha <victorrochadev0@gmail.com>
2024-05-07 21:13:24 -0300

    create first model

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 const { Router } = require("express");
-
+const usersModel = require("./apps/models/Users");
 const routes = new Router();
 
 routes.get("/", (req, res) => {
   res.send({ message: "Pega o pai" });
+});
+
+routes.get("/users", async (req, res) => {
+  const allUsers = await usersModel.findAll();
+
+  res.send({ users: allUsers });
 });
 
 module.exports = routes;
