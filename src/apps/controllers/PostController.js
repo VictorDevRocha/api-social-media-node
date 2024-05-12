2c39c680987ea30949726923737eb22f902e9fff
Victor Rocha <victorrochadev0@gmail.com>
2024-05-12 19:08:26 -0300

    Update delete post controller

--- src/apps/controllers/PostController.js
+++ src/apps/controllers/PostController.js
@@ -16,6 +16,40 @@ class postController {
 
     return res.status(200).json({ data: { image, description } });
   }
+
+  async delete(req, res) {
+    const { id } = req.params;
+
+    const verifyPost = await Posts.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!verifyPost) {
+      return res.status(400).json({ message: "Post não existe!" });
+    }
+
+    if (verifyPost.author_id != req.userId) {
+      return res
+        .status(401)
+        .json({ message: "Você não tem permissão para deletar este post!" });
+    }
+
+    const deletePost = Posts.destroy({
+      where: {
+        id,
+      },
+    });
+
+    if (!deletePost) {
+      return res.status(400).json({ message: "Falha ao deletar post!" });
+    }
+
+    return res
+      .status(200)
+      .json({ message: "post deletado com sucesso!", postId: verifyPost.id });
+  }
 }
 
 module.exports = new postController();
